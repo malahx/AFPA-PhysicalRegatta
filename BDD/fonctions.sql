668855e088ee7fa5c4e004d794c45e1fcbd0e55c
Malah <malah@neuf.fr>
2017-02-22 16:49:53 +0100

    change date Now to date Regatta

--- BDD/fonctions.sql
+++ BDD/fonctions.sql
@@ -1,9 +1,11 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `newCode`(challenge_id INT) RETURNS varchar(10) CHARSET latin1
+CREATE DEFINER=`root`@`localhost` FUNCTION `newCode`(challenge_id INT, regatta_id INT) RETURNS varchar(10) CHARSET latin1
 BEGIN
-    DECLARE chalCode VARCHAR(5);
+	DECLARE chalCode VARCHAR(5);
     DECLARE numRegatta INT(4) DEFAULT 0;
-    DECLARE dateNow DateTime DEFAULT now();
-    SELECT code INTO chalCode FROM challenge WHERE challenge.id = challenge_id;
+    DECLARE dateRegatta DATETIME;
+	SELECT code INTO chalCode FROM challenge c WHERE c.id = challenge_id;
+    SELECT date INTO dateRegatta FROM regatta r WHERE r.id = regatta_id;
     SELECT COUNT(r.id)+1 INTO numRegatta FROM regatta r INNER JOIN challenge c ON c.id = r.challenge_id WHERE c.id = 1 GROUP BY c.id;
-    RETURN CONCAT(chalCode, '-', MONTH(dateNow), '-', numRegatta);
+    
+	RETURN CONCAT(chalCode, '-', MONTH(dateRegatta), '-', numRegatta);
 END
