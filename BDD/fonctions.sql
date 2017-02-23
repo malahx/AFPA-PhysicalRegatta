6d2bf2ad03fd7e0c4d5ffec423cd00d65d9f2285
Malah <malah@neuf.fr>
2017-02-23 09:11:13 +0100

    corrected for nonexistent regatta

--- BDD/fonctions.sql
+++ BDD/fonctions.sql
@@ -1,11 +1,9 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `newCode`(challenge_id INT, regatta_id INT) RETURNS varchar(10) CHARSET latin1
+CREATE DEFINER=`root`@`localhost` FUNCTION `newCode`(challenge_id INT, regatta_date DATETIME) RETURNS varchar(10) CHARSET latin1
 BEGIN
 	DECLARE chalCode VARCHAR(5);
     DECLARE numRegatta INT(4) DEFAULT 0;
-    DECLARE dateRegatta DATETIME;
 	SELECT code INTO chalCode FROM challenge c WHERE c.id = challenge_id;
-    SELECT date INTO dateRegatta FROM regatta r WHERE r.id = regatta_id;
     SELECT COUNT(r.id)+1 INTO numRegatta FROM regatta r INNER JOIN challenge c ON c.id = r.challenge_id WHERE c.id = 1 GROUP BY c.id;
     
-	RETURN CONCAT(chalCode, '-', MONTH(dateRegatta), '-', numRegatta);
+	RETURN CONCAT(chalCode, '-', MONTH(regatta_date), '-', numRegatta);
 END
