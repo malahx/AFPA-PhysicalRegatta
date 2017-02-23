5972817ba39437f79e944e09480d3a9fb0540ef9
Malah <malah@neuf.fr>
2017-02-23 13:39:25 +0100

    added app project

---
+++ src/afpa/ecf/physicalregatta/Main.java
@@ -0,0 +1,19 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ */
+package afpa.ecf.physicalregatta;
+
+/**
+ *
+ * @author gwenole
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
