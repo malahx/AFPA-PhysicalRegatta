fcffa0cb474badadf0298cd8e5a1a953d8c23f62
Malah <malah@neuf.fr>
2017-02-23 09:33:56 +0100

    added procedure 3

--- BDD/stored procedures.sql
+++ BDD/stored procedures.sql
@@ -27,3 +27,25 @@ BEGIN
     WHERE c.sailboat_id = sailboat_id;
     
 END
+
+# Procédure 3
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAuditorFrom`(IN challenge_id INT)
+BEGIN
+	
+    SELECT r.date AS DATE, 
+		p.firstname AS AUDITOR_FIRSTNAME, 
+		p.lastname AS AUDITOR_LASTNAME, 
+		c.name AS COMMITTEE
+	FROM regatta r
+	INNER JOIN auditor a
+	ON a.id = r.auditor_id
+	INNER JOIN person p
+	ON p.id = a.person_id
+	INNER JOIN committee c
+	ON c.id = a.committee_id
+	INNER JOIN challenge cha
+	ON cha.id = r.challenge_id
+	WHERE cha.id = challenge_id;
+
+END
