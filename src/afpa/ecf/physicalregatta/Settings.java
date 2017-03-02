790ec20f0a7ade65f662991df0e644aa06b4b542
Malah <malah@neuf.fr>
2017-03-02 09:32:34 +0100

    added dao vars and jpa functions

--- src/afpa/ecf/physicalregatta/Settings.java
+++ src/afpa/ecf/physicalregatta/Settings.java
@@ -1,27 +1,46 @@
-
-
 package afpa.ecf.physicalregatta;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author Gwenole
  */
 public class Settings {
-    
+
     public static final String HOST = "10.105.49.11";
     public static final int PORT = 8080;
     public static final String PREFIX = "/api";
     public static final String GET_REGATTAS = "/currentchallenge";
-    
+    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String JDBC_URL = "jdbc:mysql://localhost/physicalregatta?noAccessToProcedureBodies=true";
+    public static final String JDBC_USER = "root";
+    public static final String JDBC_PWD = "admin";
+    private static final String PERSISTENCE_UNIT_NAME = "PhysicalRegattaPU";
+    private static EntityManagerFactory factory;
+    private static EntityManager em;
+
     public static String getUrlAPI() {
         return "http://" + HOST + ":" + PORT + PREFIX;
     }
-    
+
     public static String getUrlRegattas() {
         return getUrlAPI() + GET_REGATTAS;
     }
-    
+
     public static String getURIRegattas() {
         return PREFIX + GET_REGATTAS;
+    }
+
+    public static EntityManager getEntityManager() {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        if (em == null) {
+            em = factory.createEntityManager();
+        }
+        return em;
     }
 }
