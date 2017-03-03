8232b6398a5725bed0003808be20a4303ddffb53
Malah <malah@neuf.fr>
2017-03-03 09:43:15 +0100

    added licence

--- test/AlgoTest.java
+++ test/AlgoTest.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is free and unencumbered software released into the public domain.
  */
 
 import afpa.ecf.algo.Algo;
